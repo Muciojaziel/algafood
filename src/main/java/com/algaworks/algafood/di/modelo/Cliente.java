9fd497a38c1476ec9dd34f87e2ce3a022babf12e
Mucio Jaziel <muciojaziel@hotmail.com>
2021-09-11 16:19:20 -0300

    Adicionando classes cliente e clienteService, notificadorEmail

---
+++ src/main/java/com/algaworks/algafood/di/modelo/Cliente.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.di.modelo;
+
+public class Cliente {
+	private String nome;
+	private String email;
+	private String telefone;
+	private boolean ativo = false;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void ativar() {
+		this.ativo = true;
+	}
+
+}
