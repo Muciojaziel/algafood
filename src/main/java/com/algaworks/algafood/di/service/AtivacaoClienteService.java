863f827c6b37cde1098b49c41cdf764d87a33ad4
Mucio Jaziel <muciojaziel@hotmail.com>
2021-09-13 08:08:42 -0300

    Trabalhando com Autowired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,36 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
+@Component
 public class AtivacaoClienteService {
 	
-	private Notificador notificador;
+	@Autowired(required = false)
+	private List<Notificador> notificadores;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService:" + notificador );
-	}
+	//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
+		if(notificador != null) {
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		} else {
+			System.out.println("Não existe notificador, mas cliente foi ativado!");
+		}
 	}
+	
+//	@Autowired
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
+	}
+	
 	
 }
