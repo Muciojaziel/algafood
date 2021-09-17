cf5c9ff8f98be56e03eb35ad35c3ec8671b5a1cb
Mucio Jaziel <muciojaziel@hotmail.com>
2021-09-17 10:10:11 -0300

    Aula 3.16 resositorio de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa')
 insert into cozinha (nome) values ('Chinesa')
+
+insert into restaurante (nome, taxa_frete) values ('RestDev', 10)
+insert into restaurante (nome, taxa_frete) values ('MariRestaurante', 15)
