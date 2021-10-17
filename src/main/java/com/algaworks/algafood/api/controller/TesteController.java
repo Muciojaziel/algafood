cb4f8e8bb91b5661969f1bfc1b83c6ee34818f8e
Mucio Jaziel <muciojaziel@hotmail.com>
2021-10-17 08:06:07 -0300

    Aulas Módulo 5.20

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -80,4 +80,9 @@ public class TesteController {
                 .and(comNomeSemelhante(nome)));
     }
 
+    @GetMapping("/restaurantes/buscar-primeiro")
+    public Optional<Restaurante> restaurantesBuscarPrimeiro() {
+        return restauranteRepository.buscarPrimeiro();
+    }
+
 }
