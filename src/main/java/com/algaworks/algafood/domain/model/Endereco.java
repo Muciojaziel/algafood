33aac8ee24737b463b51114dc789c2183df8e599
Mucio Jaziel <muciojaziel@hotmail.com>
2021-10-19 07:26:38 -0300

    Aulas Módulo 6.5 Mapeando classes incorporáveis com @Embedded e @Embedable

---
+++ src/main/java/com/algaworks/algafood/domain/model/Endereco.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@Embeddable
+public class Endereco {
+
+    @Column(name = "endereco_cep")
+    private String nome;
+
+    @Column(name = "endereco_logradouro")
+    private String logradouro;
+
+    @Column(name = "endereco_numero")
+    private String numero;
+
+    @Column(name = "endereco_complemento")
+    private String complemento;
+
+    @Column(name = "endereco_bairro")
+    private String bairro;
+
+    @ManyToOne
+    @JoinColumn(name = "cidade_endereco_id")
+    private Cidade cidade;
+
+}
