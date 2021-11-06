2141178887b4bbe1d72accf4c608c78bf5721e90
Mucio Jaziel <muciojaziel@hotmail.com>
2021-11-06 00:30:07 -0300

    Aulas 7.12 Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,62 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subTotal;
+
+    @Column(nullable = false)
+    private BigDecimal taxaFrete;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    private StatusPedido status;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_cliente_id", nullable = false)
+    private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataConfirmacao;
+    private LocalDateTime dataCancelamento;
+    private LocalDateTime dataEntrega;
+
+
+
+}
+
