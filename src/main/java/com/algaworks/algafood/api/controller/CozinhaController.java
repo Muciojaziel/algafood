3932bab83cd7c72551e66c0802b8220d787a02ad
Mucio Jaziel <muciojaziel@hotmail.com>
2021-11-06 21:17:31 -0300

    Aulas 8.1 e 8.2 Exceções customizadas com @ResponseStatus

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -69,17 +69,25 @@ public class CozinhaController {
 		return ResponseEntity.notFound().build();
 	}
 	
-	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<?> remover(@PathVariable Long cozinhaId){
-		try {
+//	@DeleteMapping("/{cozinhaId}")
+//	@ResponseStatus(HttpStatus.NO_CONTENT)
+//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId){
+//		try {
+//			cadastroCozinha.excluir(cozinhaId);
+////			return ResponseEntity.noContent().build();
+//
+//		} catch (EntidadeNaoEncontradaException e) {
+//			return ResponseEntity.notFound().build();
+//
+//		} catch (EntidadeEmUsoException e) {
+//			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+//		}
+//	}
+
+		@DeleteMapping("/{cozinhaId}")
+		@ResponseStatus(HttpStatus.NO_CONTENT) // reason =  "Entidade não encontrada")
+		public void remover(@PathVariable Long cozinhaId) {
 			cadastroCozinha.excluir(cozinhaId);
-			return ResponseEntity.noContent().build();
-			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-			
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
 		}
-	}
+
 }
