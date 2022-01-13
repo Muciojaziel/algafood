dc8d61f3c99bd28c651167fa8a5c4c784cd94beb
Mucio Jaziel <muciojaziel@hotmail.com>
2022-01-13 07:57:33 -0300

    8.27. Desafio: tratando outras exceções não capturadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -8,7 +8,8 @@ public enum ProblemType {
     RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
-    PARAMETRO_INVALIDO("/parametro-invalido", "O parâmetro da URL é inválido");
+    PARAMETRO_INVALIDO("/parametro-invalido", "O parâmetro da URL é inválido"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
 
     private String title;
     private String uri;
