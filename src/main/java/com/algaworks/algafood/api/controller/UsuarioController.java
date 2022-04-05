f5723de802e2f6bbe3d1550fab60b959dc56bee0
mucio <muciojaziel@hotmail.com>
2022-04-05 08:09:09 -0300

    23.15. Desafio: refatorando serviços de usuários para usar BCrypt - ajuste

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     @Autowired
-
+    UsuarioRepository usuarioRepository;
 
     @PutMapping("/{usuarioId}/senha")
     public Usuario atualizarSenha(@PathVariable Long usuarioId, String senha){
