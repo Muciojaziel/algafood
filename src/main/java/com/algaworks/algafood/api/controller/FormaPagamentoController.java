e637b47bc29f7fa2b0dbae0472e71b69d74fd77e
mucio <muciojaziel@hotmail.com>
2022-04-09 18:08:24 -0300

    criado endpoint formas pagamentos - listar, conversor model

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.assembler.FormaPagamentoAssembler;
 import com.algaworks.algafood.api.domain.repository.FormaPagamentoRepository;
-import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.api.model.FormaPagamentoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController()
-@RequestMapping(value = "/formaspagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/formapagamento", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FormaPagamentoController {
 
     @Autowired
     FormaPagamentoRepository formaPagamentoRepository;
 
+    @Autowired
+    FormaPagamentoAssembler formaPagamentoAssembler;
+
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public List<FormaPagamento> listar() { return formaPagamentoRepository.findAll(); }
+    public List<FormaPagamentoModel> listar() { return formaPagamentoAssembler.toCollectionModel(formaPagamentoRepository.findAll()); }
+
 }
