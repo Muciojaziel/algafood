279f296ad443cda9983074841aa45efab7fbfc26
mucio <muciojaziel@hotmail.com>
2022-04-26 22:46:52 -0300

    12.13. Desafio: implementando os endpoints de produtos - ajustes na classe dominio

--- src/main/java/com/algaworks/algafood/domain/service/CadastroProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroProdutoService.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.api.model.ProdutoModel;
+import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +11,13 @@ public class CadastroProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public ProdutoModel salvar(){
-        return null;
-    }
-
+//    public Produto salvar(){
+//        return null;
+//    }
+//
+//    public Produto buscarOuFalhar(Long produtoId){
+//
+//    }
 
 
 }
