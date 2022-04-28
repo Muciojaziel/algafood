f9e085a1f7f98b86d9297cf76ec62c6bf5a0f806
mucio <muciojaziel@hotmail.com>
2022-04-28 07:12:03 -0300

    12.13. Desafio: implementando os endpoints de produtos - add conversor para modelo de dominio. - adicionado controller

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -1,17 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
+import com.algaworks.algafood.api.assembler.input.ProdutoInputDisassembler;
 import com.algaworks.algafood.api.model.ProdutoModel;
+import com.algaworks.algafood.api.model.input.ProdutoInput;
+import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +22,7 @@ import java.util.List;
 public class RestauranteProdutoController {
 
     @Autowired
-    private ProdutoRepository produtoRepository;
+    private CadastroProdutoService cadastroProdutoService;
 
     @Autowired
     private CadastroRestauranteService cadastroRestauranteService;
@@ -27,12 +30,35 @@ public class RestauranteProdutoController {
     @Autowired
     private ProdutoModelAssembler produtoModelAssembler;
 
+    @Autowired
+    private ProdutoInputDisassembler produtoInputDisassembler;
+
 
     @GetMapping
-    public List<ProdutoModel> listar(@PathVariable Long restauranteId) {
+    public List<ProdutoModel> buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 
         return produtoModelAssembler.toCollectionModel(restaurante.getProdutos());
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProdutoModel adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput){
+        Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
+
+        Produto produto = produtoInputDisassembler.toDomainObject(produtoInput);
+        produto.setRestaurante(restaurante);
+
+        return produtoModelAssembler.toModel(cadastroProdutoService.salvar(produto));
+    }
+
+    @PutMapping("/{produtoId}")
+    public ProdutoModel atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                                  @RequestBody @Valid ProdutoInput produtoInput){
+        Produto produtoAtual = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+
+        produtoInputDisassembler.copyToDomainObject(produtoInput, produtoAtual);
+
+        return produtoModelAssembler.toModel(cadastroProdutoService.salvar(produtoAtual));
+    }
 }
