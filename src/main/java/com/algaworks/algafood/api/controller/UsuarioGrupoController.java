b0357a55742c4d519db411179041119b345e3901
mucio <muciojaziel@hotmail.com>
2022-05-05 07:14:16 -0300

    12.16. Desafio: implementando os endpoints de associação de usuários com grupos - add massa de dados

---
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -0,0 +1,56 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.GrupoModelAssembler;
+import com.algaworks.algafood.api.assembler.UsuarioModelAssembler;
+import com.algaworks.algafood.api.assembler.input.UsuarioInputDissambler;
+import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.model.UsuarioModel;
+import com.algaworks.algafood.api.model.input.SenhaInput;
+import com.algaworks.algafood.api.model.input.UsuarioComSenhaInput;
+import com.algaworks.algafood.api.model.input.UsuarioInput;
+import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
+import com.algaworks.algafood.domain.service.CadastroGrupoService;
+import com.algaworks.algafood.domain.service.CadastroUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
+public class UsuarioGrupoController {
+
+    @Autowired
+    private CadastroUsuarioService cadastroUsuarioService;
+
+    @Autowired
+    private GrupoModelAssembler grupoModelAssembler;
+
+
+    @GetMapping
+    public List<GrupoModel> listar(@PathVariable Long usuarioId){
+        Usuario usuario = cadastroUsuarioService.buscarOuFalhar(usuarioId);
+
+        return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
+    }
+
+    @DeleteMapping("/{grupoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+        cadastroUsuarioService.desassociarGrupo(usuarioId, grupoId);
+    }
+
+    @PutMapping("/{grupoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+        cadastroUsuarioService.associarGrupo(usuarioId, grupoId);
+    }
+
+
+
+}
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.api.model.input.UsuarioComSenhaInput;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.UsuarioNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.GrupoRepository;
 import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -13,7 +13,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Service
@@ -23,6 +22,9 @@ public class CadastroUsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private CadastroGrupoService cadastroGrupoService;
 
     @Transactional
     public Usuario salvar(Usuario usuario){
@@ -66,4 +68,21 @@ public class CadastroUsuarioService {
         return usuarioRepository.findById(usuarioId)
                 .orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
     }
+
+    @Transactional
+    public void desassociarGrupo(Long usuarioId, Long grupoId) {
+        Usuario usuario = buscarOuFalhar(usuarioId);
+        Grupo grupo = cadastroGrupoService.buscarOuFalhar(grupoId);
+
+        usuario.removerGrupo(grupo);
+    }
+
+    @Transactional
+    public void associarGrupo(Long usuarioId, Long grupoId) {
+        Usuario usuario = buscarOuFalhar(usuarioId);
+        Grupo grupo = cadastroGrupoService.buscarOuFalhar(grupoId);
+
+        usuario.adicionarGrupo(grupo);
+    }
+
 }
