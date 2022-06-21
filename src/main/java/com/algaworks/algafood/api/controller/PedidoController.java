fc61916c9f00ee7aa180b7331ef3d28c34f0f68b
mucio <muciojaziel@hotmail.com>
2022-06-21 18:43:31 -0300

    13.6. Implementando pesquisas complexas na API

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -11,7 +11,9 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
+import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
+import com.algaworks.algafood.infrastructure.repository.spec.PedidoSpecs;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.apache.commons.lang3.StringUtils;
@@ -44,8 +46,9 @@ public class PedidoController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<PedidoResumoModel> listar(){
-        return pedidoResumoModelAssembler.toCollectionModel(pedidoRepository.findAll());
+    public List<PedidoResumoModel> pesquisar(PedidoFilter filtro){
+        List<Pedido> todosPedidos =  pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
     }
 
     @GetMapping("/{codigoPedido}")
