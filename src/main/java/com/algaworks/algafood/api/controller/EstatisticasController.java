6804127c02a5aa4c42f61954b709ae46fe314272
mucio <muciojaziel@hotmail.com>
2022-06-26 18:42:15 -0300

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,11 +3,18 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 import java.util.List;
 
@@ -18,9 +25,26 @@ public class EstatisticasController {
     @Autowired
     private VendaQueryService vendaQueryService;
 
-    @GetMapping(value = "/vendas-diarias")
+    @Autowired
+    private VendaReportService vendaReportService;
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
                                                     @RequestParam(required = false, defaultValue = "+00:00") String timeOffset){
         return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+    }
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+                                                    @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) throws JRException {
+        byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(bytesPdf);
     }
 }
